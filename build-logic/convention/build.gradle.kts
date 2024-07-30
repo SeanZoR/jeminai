0157246a72e1214a41ff20ac55cdd4a7ccf85352
Sean K <sean.katz@gmail.com>
2024-07-30 14:54:55 -0400

    Add initial project scaffolding with gradle build logic (new PR 2) (#3)
    * Setup the minimum viable Android app with gradle scaffolding for future plugins conventions
    
    * Set initial job in GH Actions
    
    * Add running tag to root README.md
    
    * Add running tag to root README.md

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.sean8.jeminai.buildlogic"
+
+// Configure the build-logic plugins to target JDK 17
+// This matches the JDK used to build the project, and is not related to what is running on device.
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "project.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
