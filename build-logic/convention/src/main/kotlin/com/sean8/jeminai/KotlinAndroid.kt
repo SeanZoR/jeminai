3ff41ea411439bddea084f4f6b61b446ea110891
sean <sean.katz@gmail.com>
2024-07-31 13:26:51 -0400

    Add launcher activity with a plain View within

--- build-logic/convention/src/main/kotlin/com/sean8/jeminai/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/sean8/jeminai/KotlinAndroid.kt
@@ -18,8 +18,8 @@ internal fun Project.configureKotlinAndroid(
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
     }
 }
